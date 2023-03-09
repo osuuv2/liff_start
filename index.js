80f10e2e07c6eb182ed36171aa9a797f87afea6a
osuuv2 <38402949+osuuv2@users.noreply.github.com>
2023-03-09 21:46:18 +0700

    1 file modified

--- index.js
+++ index.js
@@ -1,73 +1,72 @@
 // Import stylesheets
-import "./style.css"
+import './style.css';
 
 // 0. Import LIFF SDK
-import liff from "@line/liff"
+import liff from '@line/liff';
 
 // Body element
-const body = document.getElementById("body")
+const body = document.getElementById('body');
 
 // Profile elements
-const pictureUrl = document.getElementById("pictureUrl")
-const userId = document.getElementById("userId")
-const displayName = document.getElementById("displayName")
-const statusMessage = document.getElementById("statusMessage")
-const email = document.getElementById("email")
+const pictureUrl = document.getElementById('pictureUrl');
+const userId = document.getElementById('userId');
+const displayName = document.getElementById('displayName');
+const statusMessage = document.getElementById('statusMessage');
+const email = document.getElementById('email');
 
 // Button elements
-const btnShare = document.getElementById("btnShare")
-
+const btnShare = document.getElementById('btnShare');
 
 async function main() {
   // 2. liff.ready
   liff.ready.then(() => {
-    if (liff.getOS() === "android") {
-      body.style.backgroundColor = "#888888"
+    if (liff.getOS() === 'android') {
+      body.style.backgroundColor = '#888888';
     }
     if (liff.isInClient()) {
-      getUserProfile()
+      getUserProfile();
     }
-    btnShare.style.display = "block"
-  })
+    btnShare.style.display = 'block';
+  });
   // 3. Try a LIFF function
   // 4. Check where LIFF was opened
   // 5. Call getUserProfile()
   // 10. Show share button
 
   // 1. Initialize LIFF app)
-  await liff.init({liffId: "1653575653-e9vXldMN"})
+  await liff.init({ liffId: '1660703292-JNbZZWrB' });
 }
 main();
 
 // 6. Create getUserProfile()
 // *7. Get email
 async function getUserProfile() {
-  const profile = await liff.getProfile()
-  pictureUrl.src = profile.pictureUrl
-  userId.innerHTML = "<b>userId: </b>" + profile.userId
-  displayName.innerHTML = "<b>displayName: </b>" + profile.displayName
-  statusMessage.innerHTML = "<b>statusMessage: </b>" + profile.statusMessage
-  email.innerHTML = "<b>email: </b>" + liff.getDecodedIDToken().email
+  const profile = await liff.getProfile();
+  pictureUrl.src = profile.pictureUrl;
+  userId.innerHTML = '<b>userId: </b>' + profile.userId;
+  displayName.innerHTML = '<b>displayName: </b>' + profile.displayName;
+  statusMessage.innerHTML = '<b>statusMessage: </b>' + profile.statusMessage;
+  email.innerHTML = '<b>email: </b>' + liff.getDecodedIDToken().email;
 }
 
 // *8. Create shareMsg()
 async function shareMsg() {
   const result = await liff.shareTargetPicker([
     {
-      type: "text",
-      text: "This msg was shared by LIFF"
-    }
-  ])
+      type: 'text',
+      text: 'This msg was shared by LIFF',
+    },
+  ]);
   if (result) {
-    alert("Msg was shared!")
+    alert('Msg was shared!');
   } else {
-    alert("ShareTargetPicker was cancelled by user")
+    alert('ShareTargetPicker was cancelled by user');
   }
-  liff.closeWindow()
+  liff.closeWindow();
 }
 // 11. Add close window
 
 // 9. Add event listener to share button
 btnShare.onclick = () => {
-  shareMsg()
-}
+  shareMsg();
+};
